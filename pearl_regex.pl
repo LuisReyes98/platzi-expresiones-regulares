e7a3d3548ea36061bc6e627c8acb48eaede5ad41
Luis Reyes <luisrogelio98@gmail.com>
2022-03-22 11:18:13 -0400

    perl WIP

--- pearl_regex.pl
+++ pearl_regex.pl
@@ -5,3 +5,24 @@ use warnings;
 
 my $t = time;
 
+my $match = 0;
+my $nomatch = 0;
+
+open(my $f, "<./files/results.csv" ) or die ("No hay archivo");
+
+while(<$f>){
+  # Quita saltos de linea y caracteres raros de la linea
+  chomp;
+  # el doble // es para usar una expresion regular
+  if (m/^[\d]{4,4}\-02\-.*$/){
+    print $_."\n";
+    $match++;
+  } else {
+    $nomatch++;
+  }
+
+}
+
+close($f);
+
+printf("Se encontraron %d matches\d", $match);
