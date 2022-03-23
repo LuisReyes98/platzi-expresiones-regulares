75c4759bd382f494ae727b441bbb21f54357303a
Luis Reyes <luisrogelio98@gmail.com>
2022-03-23 11:20:04 -0400

    expresiones regulares en perl

--- pearl_regex.pl
+++ pearl_regex.pl
@@ -14,8 +14,16 @@ while(<$f>){
   # Quita saltos de linea y caracteres raros de la linea
   chomp;
   # el doble // es para usar una expresion regular
-  if (m/^[\d]{4,4}\-02\-.*$/){
-    print $_."\n";
+  # if (m/^[\d]{4,4}\-02\-.*$/){
+  # 1872-11-30,Scotland,England,0,0,Friendly,Glasgow,Scotland,FALSE
+  if (m/^([\d]{4,4})\-.*?,(.*?),(.*?),(\d+),(\d+),.*$/){
+
+    # veces que visitante le gano al local
+    if($5 > $4){
+      printf("%s: %s (%d) - (%d) %s \n",
+        $1, $2, $4, $5, $3
+      );
+    }
     $match++;
   } else {
     $nomatch++;
@@ -25,4 +33,4 @@ while(<$f>){
 
 close($f);
 
-printf("Se encontraron %d matches\d", $match);
+printf("Se encontraron \n - %d matches\n - %d no matches \ntardo %d segundos", $match, $nomatch, time() - $t);
